cec1da490bc2a717ba288565c6a79a006bb6ae41
Shivaprasad <shivaprasad.bhat@learner.manipal.edu>
2020-07-15 01:09:48 +0530

    adding profile code

--- src/scripts/php/reset_password.php
+++ src/scripts/php/reset_password.php
@@ -1,30 +1,35 @@
 <?php
 if (!isset($_SESSION['user'])) session_start();
 if (isset($_POST['reset'])) {
-    $password = trim($_POST['pass']);
-    $cpassword = trim($_POST['cpass']);
-    if ($password === $cpassword) {
+    $pass = trim($_POST['pass']);
+    $cpass = trim($_POST['cpass']);
+    if ($pass === $cpass) {
         $username = $_SESSION['user'];
         require('./connect.php');
         $conn->select_db("mysql");
-        $query = "ALTER USER '$username'@'localhost' IDENTIFIED BY '$password'";
+        $query = "ALTER USER '$username'@'localhost' IDENTIFIED BY '$pass'";
+
         $result = $conn->query($query);
 
-        if ($result === TRUE) {
-            $conn->close();
-            echo '
-                <script language="javascript">
-                alert("Password Updated")
-                window.location.href="/cvd/src/views/priofile.php"
-                </script>';
-        } else {
-            $conn->close();
-            echo '
-                <script language="javascript">
-                alert("Something Went Wrong")
-                window.location.href="/cvd/src/views/priofile.php"
-                </script>';
+        if ($result === FALSE) {
+            echo $conn->error;
         }
+        $conn->close();
+        // if ($result === TRUE) {
+        //     $conn->close();
+        //     echo '
+        //         <script language="javascript">
+        //         alert("Password Updated")
+        //         window.location.href="/cvd/src/views/priofile.php"
+        //         </script>';
+        // } else {
+        //     $conn->close();
+        //     echo '
+        //         <script language="javascript">
+        //         alert("Something Went Wrong")
+        //         window.location.href="/cvd/src/views/priofile.php"
+        //         </script>';
+        // }
     } else {
         echo '
                 <script language="javascript">
